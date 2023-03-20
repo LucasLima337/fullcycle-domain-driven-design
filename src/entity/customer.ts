a39db8ddd9f89ea5f99a93a89502bb25a66615a6
Lucas Lima <luc.tech3337@gmail.com>
2023-03-19 22:19:43 -0300

    chore: update function in customer class

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,13 +1,13 @@
 class Customer {
   private _id: string;
   private _name: string;
-  private _address: string;
+  private _address!: string;
   private _active = false;
 
-  constructor (id: string, name: string, address: string) {
+  constructor (id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validate();
   }
 
   get id(): string {
@@ -17,24 +17,39 @@ class Customer {
   get name(): string {
     return this._name;
   }
-  
-  changeName(name: string): void {
-    this._name = name;
-  }
 
   get address(): string {
     return this._address;
   }
 
-  set address(address: string) {
+  changeAddress(address: string): void {
     this._address = address;
   }
 
+  changeName(name: string): void {
+    this._name = name;
+    this.validate();
+  }
+
   activate(): void {
+    if (this._address === undefined) {
+      throw new Error("Address is required to activate a customer");
+    }
+
     this._active = true;
   }
 
   deactivate(): void {
     this._active = false;
+  }
+
+  validate(): void {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
   }
 }
