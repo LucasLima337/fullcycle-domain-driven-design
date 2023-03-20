a00a2b4ff4666003c98b53a77069518baf1a5276
Lucas Lima <luc.tech3337@gmail.com>
2023-03-19 21:10:12 -0300

    chore: add properties to customer class

---
+++ src/entity/customer.ts
@@ -0,0 +1,40 @@
+class Customer {
+  private _id: string;
+  private _name: string;
+  private _address: string;
+  private _active = false;
+
+  constructor (id: string, name: string, address: string) {
+    this._id = id;
+    this._name = name;
+    this._address = address;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  
+  changeName(name: string) {
+    this._name = name;
+  }
+
+  get address(): string {
+    return this._address;
+  }
+
+  set address(address: string) {
+    this._address = address;
+  }
+
+  activate() {
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+}
