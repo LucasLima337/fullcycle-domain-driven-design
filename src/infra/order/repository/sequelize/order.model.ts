bea04f7a50710de57662f6bfe8366e74919a0c83
Lucas Lima <luc.tech3337@gmail.com>
2023-05-01 18:06:01 -0300

    feat: create order models

---
+++ src/infra/order/repository/sequelize/order.model.ts
@@ -0,0 +1,23 @@
+import { Table, Column, PrimaryKey, Model, ForeignKey, BelongsTo, HasMany } from "sequelize-typescript";
+import CustomerModel from "../../../customer/repository/sequelize/customer.model";
+import OrderItemModel from "./orderItem.model";
+
+@Table({ tableName: "orders", timestamps: false })
+export default class OrderModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: string;
+
+  @ForeignKey(() => CustomerModel)
+  @Column({ allowNull: false })
+  declare customerId: string;
+
+  @BelongsTo(() => CustomerModel)
+  declare customer: CustomerModel;
+
+  @HasMany(() => OrderItemModel)
+  declare items: OrderItemModel[];
+
+  @Column({ allowNull: false })
+  declare total: number;
+}
---
+++ src/infra/order/repository/sequelize/orderItem.model.ts
@@ -0,0 +1,36 @@
+import { Table, Column, PrimaryKey, Model, ForeignKey, BelongsTo } from "sequelize-typescript";
+import ProductModel from "../../../product/repository/sequelize/product.model";
+import OrderModel from "./order.model";
+
+@Table({ tableName: "orderItems", timestamps: false })
+export default class OrderItemModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: string;
+
+  @ForeignKey(() => ProductModel)
+  @Column({ allowNull: false })
+  declare productId: string;
+
+  @BelongsTo(() => ProductModel)
+  declare product: ProductModel;
+
+  @ForeignKey(() => OrderModel)
+  @Column({ allowNull: false })
+  declare orderId: string;
+
+  @BelongsTo(() => OrderModel)
+  declare order: OrderModel;
+
+  @Column({ allowNull: false })
+  declare name: string;
+  
+  @Column({ allowNull: false })
+  declare price: number;
+  
+  @Column({ allowNull: false })
+  declare quantity: number;
+  
+  @Column({ allowNull: false })
+  declare total: number;
+}
